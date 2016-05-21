554ca0c27f582d5303fbc1bd4d4cb0f099577c3d
tokkie07 <not.too.late0724@gmail.com>
2016-05-21 16:56:49 +0900

    Update numerics_spec.rb

--- spec/numerics_spec.rb
+++ spec/numerics_spec.rb
@@ -12,4 +12,16 @@ describe Numerics do
       Numerics.odd?(4) == false
     }
   end
+  
+  if 'even test (false)' do
+     assert {
+      Numerics.even?(3) == false
+     }
+  end
+  
+  if 'even test (true)' do
+     assert {
+      Numerics.even?(4) == true
+     }
+  end
 end
